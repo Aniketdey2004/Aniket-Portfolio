7780e8e6e37badab73871afbb1f18944ae0fdc67
Aniket Dey <aniketdey2004@gmail.com>
2026-06-27 08:41:22 +0530

    gihtub service fixed

--- backend/src/services/github.service.js
+++ backend/src/services/github.service.js
@@ -2,8 +2,10 @@ const axios = require('axios');
 const U = () => process.env.GITHUB_USERNAME || 'Aniketdey2004';
 
 exports.getProfile = async () => {
-  const { data } = await axios.get(`https://api.github.com/users/${U()}`, { timeout: 15000 });
-  console.log("I am a github error please checdk me",data);
+  const { data } = await axios.get(`https://api.github.com/users/${U()}`, { headers:{
+    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    Accept: "application/vnd.github+json",
+  } });
   return {
     username: data.login,
     name: data.name,
